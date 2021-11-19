2b0e739baa7cf802efaa33e2a5f8a686852efeec
Mare <remg413@gmail.com>
2021-11-19 17:06:08 -0600

    Peticiones Get de Personal, Arneses, MO y Registros terminadas.

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -11,13 +11,54 @@ import { ApiGolletService } from '../services/api-gollet.service';
 export class FolderPage implements OnInit {
   public folder: string;
 
+  Personal: any[] = [];
+  Arneses: any[] = [];
+  MO: any[] = [];
+  Actividades: any[] = [];
+  Registros: any[] = [];
+  
+  personal = false;
+  arneses = false;
+  mo = false;
+  actividades = false;
+  registros =  false;
+  
   constructor(private activatedRoute: ActivatedRoute,
               private apiGollet: ApiGolletService) { 
-                // apiGollet.getPersonal();
-              }
+    apiGollet.getPersonal()
+      .subscribe( ( data: any ) => {
+        console.log( data.personal );
+        this.Personal = data.personal;
+      } );
+    apiGollet.getArneses()
+      .subscribe( ( data: any ) => {
+        console.log( data.arneses );
+        this.Arneses = data.arneses;
+      } );
+    apiGollet.getMO()
+      .subscribe( ( data: any ) => {
+        console.log( data.MOs );
+        this.MO = data.MOs;
+      } );
+    // apiGollet.getActividades()
+    //   .subscribe( ( data: any ) => {
+    //     console.log( data );
+    //   } );
+    apiGollet.getRegistros()
+      .subscribe( ( data: any ) => {
+        console.log( data.Registros );
+        this.Registros = data.Registros;
+      } )
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.folder == 'Personal' ? this.personal = true : this.personal = false;
+    this.folder == 'Arneses' ? this.arneses = true : this.arneses = false;
+    this.folder == 'MO' ? this.mo = true : this.mo = false;
+    this.folder == 'Actividades' ? this.actividades = true : this.actividades = false;
+    this.folder == 'Registros' ? this.registros = true : this.registros = false;
   }
 
 }
--- src/app/services/api-gollet.service.ts
+++ src/app/services/api-gollet.service.ts
@@ -1,19 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// import { HTTP } from '@ionic-native/http/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiGolletService {
   
-  constructor( /*private http: HttpClient*/ ) { 
+  // constructor( private http: HTTP ) { 
+  //   console.log( 'api-gollet listo.' );
+  // }
+
+  constructor( private http: HttpClient ) { 
     console.log( 'api-gollet listo.' );
+  }
+
+  getPersonal() {
+    return this.http.get( 'https://gollet-electronics.herokuapp.com/api/personal' );
+  }
+
+  getArneses() {
+    return this.http.get( 'https://gollet-electronics.herokuapp.com/api/arneses' );
+  }
+
+  getMO() {
+    return this.http.get( 'https://gollet-electronics.herokuapp.com/api/mo' );
+  }
+
+  getActividades() {
+    // return this.http.get( 'https://gollet-electronics.herokuapp.com/api/' );
+  }
+
+  getRegistros() {
+    return this.http.get( 'https://gollet-electronics.herokuapp.com/api/registro' );
   }
   
   // getPersonal() {
-  //   this.http.get( 'https://gollet-electronics.herokuapp.com/api/personal' )
-  //       .subscribe( personal => {
-  //         console.log( personal );
+  //   this.http.get( 'https://gollet-electronics.herokuapp.com/api/personal', {}, {} )
+  //       .then( data => {
+  //         console.log( data.status );
+  //         console.log( data.data );
+  //         console.log( data.headers );
   //       } )
+  //       .catch( error => {
+  //         console.log( error.status );
+  //         console.log( error.error );
+  //         console.log( error.headers );
+  //       } );
   // }
 }
